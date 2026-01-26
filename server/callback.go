6ad121778c928467e548f82937071717dcfa504d
angpaoprw <55875113+angpaoprw@users.noreply.github.com>
2026-01-27 01:28:19 +0700

    fix: change get to post

--- server/callback.go
+++ server/callback.go
@@ -3,7 +3,7 @@ package server
 func (s *App) callbackRoute() {
 	callback := s.App.Group("/callback")
 	{
-		callback.Get("/alchemy", s.Controller.AlchemyCallback)
+		callback.Post("/alchemy", s.Controller.AlchemyCallback)
 	}
 
 }
