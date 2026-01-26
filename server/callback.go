925c071b62abcc08b7230420d30fca7ef5a46676
angpaoprw <55875113+angpaoprw@users.noreply.github.com>
2026-01-27 01:35:04 +0700

    fix: prefix name on callback

--- server/callback.go
+++ server/callback.go
@@ -1,7 +1,7 @@
 package server
 
 func (s *App) callbackRoute() {
-	callback := s.App.Group("/callback")
+	callback := s.Route.Group("/callback")
 	{
 		callback.Post("/alchemy", s.Controller.AlchemyCallback)
 	}
