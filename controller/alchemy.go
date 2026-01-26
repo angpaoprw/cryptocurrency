74033b75b8283d46cffdbc17a1b452dc5351966b
angpaoprw <55875113+angpaoprw@users.noreply.github.com>
2026-01-27 01:20:43 +0700

    add: log for alchemy callback

--- controller/alchemy.go
+++ controller/alchemy.go
@@ -1,6 +1,9 @@
 package controller
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+)
 
 // AlchemyCallback godoc
 // @Summary Alchemy Webhook Callback
@@ -11,6 +14,13 @@ import "github.com/gofiber/fiber/v2"
 // @Success 200 {string} string "Alchemy Callback"
 // @Router /callback/alchemy [get]
 func (s *Controller) AlchemyCallback(c *fiber.Ctx) error {
+	// Log the incoming request body
+	body := c.Body()
+	zap.L().Info("Alchemy callback received",
+		zap.String("body", string(body)),
+		zap.String("method", c.Method()),
+		zap.String("path", c.Path()),
+	)
 
 	return c.SendString("Alchemy Callback")
 }
